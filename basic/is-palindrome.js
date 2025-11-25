457591a2045fcb2f2f09bd00237a973781c0a276
bhav2028 <bytalele2002@gmail.com>
2025-11-25 18:25:56 +0530

    Added Check if string is palindrome problem

---
+++ basic/is-palindrome.js
@@ -0,0 +1,14 @@
+// Check if the given string is a palindrome
+
+function isPalindrome(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("better"));
+console.log(isPalindrome("1234321"));
